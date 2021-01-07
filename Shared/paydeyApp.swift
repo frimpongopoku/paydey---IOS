53c5b363d45e481f7a8b5dc875cf424d279e47ac
frimpongopoku <pongofrimi@gmail.com>
2021-01-07 16:45:08 +0400

    Initial Commit

---
+++ Shared/paydeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  paydeyApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 07/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct paydeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
